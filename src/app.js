ab5faedeee497a3269e6fd7066cc05f9fd57a753
Rodiev <rodiev.gaf@gmail.com>
2023-04-20 21:42:44 +0300

    setup of DevTools

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { DevTools } from './utils/index';
+
+export default class App extends React.Component {
+    static path = '/';
+
+    render() {
+        return (
+            <div>
+                 <h1> Hello World </h1>
+                { process.env.NODE_ENV !== 'production' ? <DevTools /> : null}
+            </div>
+        );
+    }
+}
